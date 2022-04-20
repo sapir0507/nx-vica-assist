46f93052e22bf0535b30545da9c86bd03df80aa4
sapir shahar <sapir.s@info.cts.org.il>
2022-04-21 00:29:36 +0300

    hotel service & interfaces\

---
+++ libs/ui/src/lib/flight.interface.ts
@@ -0,0 +1,24 @@
+export interface Flight {
+    id: number,
+    Passangers?: number ,
+    Dest?: string ,
+    Org?: string ,
+    Return?: string ,
+    Departure?: string ,
+    Airport?: string ,
+    Airways?: string ,
+    Info?: string ,
+    Price?: number 
+}
+
+export interface FlightRequest {
+    Passangers?: number ,
+    Dest?: string ,
+    Org?: string ,
+    Return?: string ,
+    Departure?: string ,
+    Airport?: string ,
+    Airways?: string ,
+    Info?: string ,
+    Price?: number 
+}
---
+++ libs/ui/src/lib/hotel.interface.ts
@@ -0,0 +1,16 @@
+export interface Hotel {
+    id: number,
+    Name?:  string,
+    stars?: number,
+    breakfast?: boolean,
+    bedsIncluded?: string,
+    price?: number
+}
+
+export interface HotelRequest {
+    Name?:  string,
+    stars?: number,
+    breakfast?: boolean,
+    bedsIncluded?: string,
+    price?: number
+}
